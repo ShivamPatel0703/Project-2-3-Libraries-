fd126a467324b5a4693922f39a9788225a701894
ShivamPatel0703 <79234521+ShivamPatel0703@users.noreply.github.com>
2021-05-03 10:27:05 -0400

    Update fakerLib2.py

--- fakerLib2.py
+++ fakerLib2.py
@@ -1,1 +1,10 @@
-import fakeredis
+import sys
+from PIL import Image, ImageFilter
+try:
+    tigerpic = Image.open("tigerpic.jpeg")
+except IOError:
+    print("Image didn't load")
+tigerpic.show()
+# Print the Tiger Pic information (size and format)
+print(tigerpic.size)
+print(tigerpic.format)
